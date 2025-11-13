41b89cd1a4751381ab1210e410f9284ec9ef1e62
monicaCurso2023 <monicacurso2023@gmail.com>
2025-11-13 12:15:36 +0100

    elimino parte de import

--- EJERC-ARRAY-OBJETOS/index.js
+++ EJERC-ARRAY-OBJETOS/index.js
@@ -1,12 +1,6 @@
 import { datosCasas } from './constants.js';
 
-const lista = document.getElementById('lista');
 
-datosCasas.forEach(casa => {
-  const li = document.createElement('li');
-  li.textContent = `${casa.donoCasa} tiene ${casa.numPortas} puertas.`;
-  lista.appendChild(li);
-});
 
 
 //1. Lista de donos
@@ -14,3 +8,7 @@ datosCasas.forEach(casa => {
 for (let casa of datosCasas) {
     console.log(casa.donoCasa);
 }
+
+
+
+
